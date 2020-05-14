e7f5e3d4e8cc01023f051859e6628aaf7078d978
oreshkindev <oreshkin.dev@outlook.com>
2020-05-14 22:27:58 +0400

    package

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    productionSourceMap: false,
+    pluginOptions: {
+        i18n: {
+            locale: 'ru',
+            fallbackLocale: 'en',
+            localeDir: 'locales',
+            enableInSFC: false,
+        },
+    },
+}
